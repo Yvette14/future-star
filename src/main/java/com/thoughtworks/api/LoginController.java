9c96d96bfc66f637b6c827d8022c1e818f4b1266
Yvette14 <libra1014tyb@163.com>
2017-09-24 23:02:21 +0800

    add login exception

--- src/main/java/com/thoughtworks/api/LoginController.java
+++ src/main/java/com/thoughtworks/api/LoginController.java
@@ -1,14 +1,12 @@
 package com.thoughtworks.api;
 
 import com.thoughtworks.dto.User;
+import com.thoughtworks.exception.InvalidCredentialException;
 import com.thoughtworks.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/login")
@@ -18,10 +16,11 @@ public class LoginController {
     LoginService loginService;
 
     @PostMapping
-    public ResponseEntity<?> login(@RequestBody User user) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public String login(@RequestBody User user) {
         if (loginService.login(user)) {
-            return new ResponseEntity<>("login successfully!", HttpStatus.CREATED);
+            return "login successfully!";
         }
-        return new ResponseEntity<>("login failed!", HttpStatus.UNAUTHORIZED);
+        throw new InvalidCredentialException("login failed!");
     }
 }
